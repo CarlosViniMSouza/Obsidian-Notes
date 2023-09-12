12f01d26a8701eacb08da9c763b6a6b8058e8259
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-09-12 12:10:42 -0300

    update: notas-Disp-Moveis

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,5 @@
-git init
 git add -A
-git commit -m "Add First Note"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/Obsidian-Notes.git
+git commit -m "update: notas-Disp-Moveis"
 git push -u origin main
 git status
+
